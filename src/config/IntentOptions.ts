e79d4c765824d88736361dd392acb0994ec68336
EJ <ej@LAPTOP-K4C8F2EG>
2024-10-04 17:04:46 +0800

    連接discord bot 建立.env

---
+++ src/config/IntentOptions.ts
@@ -0,0 +1,4 @@
+import { GatewayIntentsString } from "discord.js";
+
+
+export const IntentOptions : GatewayIntentsString[] = ["Guilds"];
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { Client	} from "discord.js";
+import { IntentOptions } from "./config/IntentOptions";	// Importing the constant
+import * as dotenv from "dotenv";
+dotenv.config();
+
 
 (async () => {
-	const Bot =  new Client();
+	const Bot =  new Client({ intents: IntentOptions });	// Using the constant		
 	await Bot.login(process.env.BOT_TOKEN);
+	Bot.on("ready", () => console.log('${Bot.user.tag} is ready!'));
 })();
